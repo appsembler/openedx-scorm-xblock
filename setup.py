4c7fc714905500ae20faf1c614e0db9317c5686a
Bryan Wilson <bryanlandia@gmail.com>
2023-11-13 10:09:41 -0800

    v 16.0.0-appsembler-scorablexblockmixin

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with io.open(os.path.join(here, "README.rst"), "rt", encoding="utf8") as f:
 
 setup(
     name="openedx-scorm-xblock",
-    version="16.0.0-appsembler-juniper-compat",
+    version="16.0.0-appsembler-scorablexblockmixin",
     description="Scorm XBlock for Open edX",
     long_description=readme,
     long_description_content_type="text/x-rst",
